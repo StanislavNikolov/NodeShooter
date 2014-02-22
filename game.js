25fbdff7629a5306a09f0783a984dded02a7e962
viktorTrz <viktor_trz@abv.bg>
2014-02-22 14:36:52 +0200

    risuvane na obli steni

--- game.js
+++ game.js
@@ -40,14 +40,40 @@ window.addEventListener("keyup", function (args)
 {
     keys[args.keyCode] = false;
 }, false);
-
+function drawWall(current){
+	
+	context.fillStyle = "green";
+	context.beginPath();
+	context.moveTo(current.x+Math.cos(current.startAngle)*current.inerRadius,current.y+Math.sin(i+current.startAngle)*current.inerRadius);
+	for (var i = current.startAngle ; i <= current.finishAngle;i += Math.abs(current.finishAngle-current.startAngle)/50) {
+		context.lineTo(current.x+Math.cos(i)*current.inerRadius,current.y+Math.sin(i)*current.inerRadius);
+	}
+	for (var i = current.finishAngle ; i >= current.startAngle;i -= Math.abs(current.finishAngle-current.startAngle)/50) {
+		context.lineTo(current.x+Math.cos(i)*current.outerRadius,current.y+Math.sin(i)*current.outerRadius);
+	}
+	context.closePath();
+	context.fill();
+	context.beginPath();
+	context.arc(current.x+(Math.cos(current.startAngle)*(Math.abs(current.outerRadius-current.inerRadius)/2+current.inerRadius)),
+						current.y+Math.sin(current.startAngle)*(Math.abs(current.outerRadius-current.inerRadius)/2+current.inerRadius),
+									Math.abs(current.outerRadius-current.inerRadius)/2,0,2*Math.PI);
+	context.closePath();
+	context.fill();
+	context.beginPath();
+	context.arc(current.x+(Math.cos(current.finishAngle)*(Math.abs(current.outerRadius-current.inerRadius)/2+current.inerRadius)),
+						current.y+Math.sin(current.finishAngle)*(Math.abs(current.outerRadius-current.inerRadius)/2+current.inerRadius),
+									Math.abs(current.outerRadius-current.inerRadius)/2,0,2*Math.PI);
+	context.closePath();
+	context.fill();
+	
+}
 function draw() // moje bi edinstvenoto koeto pravi game.js
 {	
 	context.clearRect(0,0,canvas.width,canvas.height);
 
 	if(myself != undefined)
 	{
-		for (var i =0 ;i < players.length ; i++)
+		for ( var i = 0 ; i < players.length ; i ++ )
 		{
 			context.fillStyle = "red";
 			if (players[i].simpleid == myself.simpleid)
@@ -55,7 +81,13 @@ function draw() // moje bi edinstvenoto koeto pravi game.js
 			context.fillRect(players[i].pos.x, players[i].pos.y, 10, 10);
 		}
 	}
-
+	
+	i = undefined;
+	
+	testWall = {startAngle:Math.PI/2,finishAngle:Math.PI*3/2,inerRadius:30,outerRadius:50,x:400,y:400}
+	drawWall(testWall);
+	
+	
 	context.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
