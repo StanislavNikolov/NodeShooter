88463ab15856ab930feaed9e2d9d6635392be948
viktorTrz <viktor_trz@abv.bg>
2014-03-03 13:00:14 +0200

    fiziki na bullet-ite

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ function inWall(p)
 			if (angle>walls[j].angle.start && angle<walls[j].angle.finish)
 			{
 				console.log ("distance:",distanceBetween(walls[j].pos,p.pos),walls[j].radius.iner );
-				if (distanceBetween(walls[j].pos,p.pos)<walls[j].radius.iner)
+				if (distanceBetween(walls[j].pos,p.pos)<(walls[j].radius.iner+walls[j].radius.outer)/2)
 					return {index: j, partCollided: {pos: walls[j].pos, radius: walls[j].radius.iner, inIner: 1}};
 				else 
 					return {index: j, partCollided: {pos: walls[j].pos, radius: walls[j].radius.outer, inIner: 0}};
@@ -182,6 +182,23 @@ function putOutOf(o1,o2,distance){
 	o1.pos.x = o2.pos.x + (o1.pos.x-o2.pos.x)*distance/(distanceBetween(o1.pos,o2.pos));
 	o1.pos.y = o2.pos.y + (o1.pos.y-o2.pos.y)*distance/(distanceBetween(o1.pos,o2.pos));
 }
+function findNewAngle (p,w){
+	var vx=p.d.x,vy=p.d.y,tpx=(w.pos.x-p.pos.x),tpy=(w.pos.y-p.pos.y),p;
+	var bx = w.pos.x,by = w.pos.y, px = p.pos.x , py = p.pos.y;
+
+	p = 2*(vx*tpx+vy*tpy)/(tpx*tpx+tpy*tpy);
+	vx=(vx-p*tpx);//
+	vy=(vy-p*tpy);//*(Math.abs(vy*tpx-vx*tpy)/(0.1+0.9*Math.sqrt(vx*vx+vy*vy)*Math.sqrt(tpx*tpx+tpy*tpy)));
+	if (vy>0)
+	{
+		return Math.acos(vx/(Math.sqrt(vx*vx+vy*vy)));
+	}
+	else 
+	{
+		return Math.PI*2-Math.acos(vx/(Math.sqrt(vx*vx+vy*vy)));
+	}
+				
+}
 function movePlayers()
 {
 	for(var i = 0;i < players.length;i ++)
@@ -189,39 +206,18 @@ function movePlayers()
 		if(players[i].speed > 0.01 || players[i].speed < -0.01)
 		{	
 		
-			
 			if(inWall(players[i]).index != -1){
 				//tuka she ima nqkvi magii
 				
-				var index = inWall(players[i]).index, objectCollided = inWall(players[i]).partCollided;
-				var vx=players[i].d.x,vy=players[i].d.y,tpx=(objectCollided.pos.x-players[i].pos.x),tpy=(objectCollided.pos.y-players[i].pos.y),p,increasement=0;
-				var bx = objectCollided.pos.x,by = objectCollided.pos.y, r1 = players[i].radius + 1, r2 = objectCollided.radius, px = players[i].pos.x , py = players[i].pos.y;
+				var index = inWall(players[i]).index, objectCollided = inWall(players[i]).partCollided,r1 = players[i].radius + 1, r2 = objectCollided.radius;
+				
 				if (!objectCollided.inIner)
-				{
 					putOutOf(players[i],objectCollided,r1+r2);
-				}
 				else
-				{	
 					putOutOf(players[i],objectCollided,r2-r1);
-				}
 					
-				p = 2*(vx*tpx+vy*tpy)/(tpx*tpx+tpy*tpy);
-				players[i].d.x=(vx-p*tpx);//
-				players[i].d.y=(vy-p*tpy);//*(Math.abs(vy*tpx-vx*tpy)/(0.1+0.9*Math.sqrt(vx*vx+vy*vy)*Math.sqrt(tpx*tpx+tpy*tpy)));
 				players[i].speed *= 0.8;
-					
-				if (players[i].d.y>0)
-				{
-					players[i].rotation = Math.acos(players[i].d.x/(Math.sqrt(players[i].d.x*players[i].d.x+players[i].d.y*players[i].d.y)));
-				}
-				else 
-				{
-					players[i].rotation = Math.PI*2-Math.acos(players[i].d.x/(Math.sqrt(players[i].d.x*players[i].d.x+players[i].d.y*players[i].d.y)));
-				
-				}
-				
-				
-				console.log("p:",p,"dX:",players[i].d.x,"dY:",players[i].d.y,"rotation:",players[i].rotation);
+				players[i].rotation = findNewAngle(players[i],objectCollided);
 				
 			}
 			else
@@ -244,8 +240,10 @@ function moveBullets()
 	{
 		var collision = false;
 		bullets[i].radius -= 0.004;
-		bullets[i].pos.x += Math.cos(bullets[i].rotation) * 6;
-		bullets[i].pos.y += Math.sin(bullets[i].rotation) * 6;
+		bullets[i].d.x = Math.cos(bullets[i].rotation) * 6;
+		bullets[i].d.y = Math.sin(bullets[i].rotation) * 6;
+		bullets[i].pos.x += bullets[i].d.x;
+		bullets[i].pos.y += bullets[i].d.y;
 
 		for(var j = 0;j < players.length;j ++)
 		{
@@ -267,7 +265,16 @@ function moveBullets()
 			i --;
 		}else {
 			if (inWall(bullets[i]).index!=-1){
-				bullets[i].rotation+=Math.PI;	
+				
+				var index = inWall(bullets[i]).index, objectCollided = inWall(bullets[i]).partCollided,r1 = bullets[i].radius + 1, r2 = objectCollided.radius;
+				
+				if (!objectCollided.inIner)
+					putOutOf(bullets[i],objectCollided,r1+r2);
+				else
+					putOutOf(bullets[i],objectCollided,r2-r1);
+					
+				bullets[i].rotation = findNewAngle(bullets[i],objectCollided);
+			
 			}
 		}
 	}
@@ -348,5 +355,6 @@ function Bullet(x, y, r, shr, damage)
 	this.radius = 2;
 	this.simpleid = ++ nextIndex;
 	this.shooter = shr;
+	this.d = new Vector(Math.cos(r),Math.sin(r));
 	this.damage = damage;
 }
