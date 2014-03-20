fb2dd816e241895bbc9131c1a631639d65fce785
Stanislav Ch. Nikolov <stanislav@cukii.tk>
2014-03-20 21:44:31 +0200

    Migrirame kum po-burzata sistema s mapove i obhojdane s iteratori

--- client.js
+++ client.js
@@ -6,40 +6,40 @@ while(loginName == "" || loginName.length > 12)
 
 var serverIP = "localhost"; // Тук се настроива ип-то на сървъра
 
-var socket = io.connect(serverIP);//pravq socket za vruzka s server-a
+var socket = io.connect(serverIP);// сокет за връзка със сървъра
 console.log("Sending login info...");
 socket.emit("login", {name: loginName });
 console.log("Login info sent.");
 
-socket.on("updateUserInformation", function (data) // servera kazva za promqna na nqkoi user
+socket.on("updatePlayerInformation", function (data)
 {
-	var index = indexOf(data.simpleid);
 	if(data.pos != undefined)
-		players[index].pos = data.pos;
+		players[data.sid].player.pos = data.pos;
 	if(data.rotation != undefined)
-		players[index].rotation = data.rotation; 
+		players[data.sid].player.rotation = data.rotation; 
 	if(data.radius != undefined)
-		players[index].radius = data.radius; 
+		players[data.sid].player.radius = data.radius; 
 	if(data.hp != undefined)
-		players[index].hp = data.hp; 
+		players[data.sid].player.hp = data.hp; 
 	if(data.dead != undefined)
-		players[index].dead = data.dead; 
+		players[data.sid].player.dead = data.dead; 
 });
 socket.on("updateBulletInformation", function (data)
 {
-	var index = indexOf(data.simpleid,"bullet");
 	if(data.pos != undefined)
-		bullets[index].pos = data.pos;
+		bullets[data.sid].player.pos = data.pos;
 	if(data.rotation != undefined)
-		bullets[index].rotation = data.rotation; 
+		bullets[data.sid].player.rotation = data.rotation; 
 	if(data.radius != undefined)
-		bullets[index].radius = data.radius; 
+		bullets[data.sid].player.radius = data.radius; 
 });
 
-socket.on("initNewUser", function (data) // kogato nqkoi se logne, survara mi go prashta za da go dobavq
+socket.on("initNewPlayer", function (data) // kogato nqkoi se logne, survara mi go prashta za da go dobavq
 {
-	console.log("Received initNewUser event!");
-	players.push(data);
+	console.log(data);
+	players[data.sid] = {}; 
+	players[data.sid].player = data.player;
+	players[data.sid].socket = {};
 });
 socket.on("initNewWall", function (data)
 {
@@ -48,30 +48,27 @@ socket.on("initNewWall", function (data)
 socket.on("removeUser", function (data) // kogato nqkoi se disconnectne, go maham
 {
 	console.log("Received removeUser event!");
-	players.splice( indexOf(data.simpleid), 1 );
+	delete players[data.sid];
 });
 socket.on("removeBullet", function (data) // kogato nqkoi se disconnectne, go maham
 {
-	bullets.splice( indexOf(data.simpleid, "bullet"), 1 );
+	bullets.splice( data.sidOf(data.simpleid, "bullet"), 1 );
 });
 socket.on("playerShooted", function (data) // kogato nqkoi se disconnectne, go maham
 {
-	console.log(frame - data.frame);
-	var index = indexOf(data.psimpleid);
-	bullets.push(new Bullet(  players[index].pos.x, 
-		players[index].pos.y, players[index].rotation, data.bsimpleid  ));
+	//bullets.push(new Bullet(  players[data.sid].pos.x, 
+	//	players[data.sid].pos.y, players[data.sid].rotation, data.bsimpleid  ));
 });
 
 socket.on("initNewBullet", function (data) // kogato nqkoi se disconnectne, go maham
 {
-	bullets.push(new Bullet( data.pos.x, data.pos.y, data.rotation, data.simpleid ));
+	//bullets.push(new Bullet( data.pos.x, data.pos.y, data.rotation, data.simpleid ));
 });
 
-socket.on("joinGame", function (data) // ako sum poluchil tova, znachi drugite me vijdat
+socket.on("joinGame", function (data) // значи: "Ок, всички вече те знаят, влизай и ти"
 {
 	console.log("Received joinGame event!");
-	myself = players[indexOf(data.simpleid)]; // za da imam referenciq kum sebesi
-	frame = data.frame;
+	myself = players[data.sid].player; // за да знам точно кой съм аз0
 });
 
 function sendMoveRequest()
@@ -98,4 +95,3 @@ function sendShootRequest()
 
 setInterval(sendMoveRequest, 50);
 setInterval(sendShootRequest, 20);
-setInterval(function nextFrame() {frame ++;}, 20);
